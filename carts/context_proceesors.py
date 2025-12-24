aecb40338365867aeb1d835f7493be010c4b7ec9
ayush-m1911 <ayushmetkar3@gmail.com>
2025-12-24 13:22:47 +0530

    Now Cart page can be accessed by Cart Icon

---
+++ carts/context_proceesors.py
@@ -0,0 +1,16 @@
+from .models import Cart, CartItem
+from .views import _cart_id
+
+def counter(request):
+ if 'admin' in request.path:
+        return {}
+ else:
+    cart_count = 0
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.all().filter(cart=cart)
+        for cart_item in cart_items:
+            cart_count += cart_item.quantity
+    except Cart.DoesNotExist:
+        cart_count = 0
+    return dict(cart_count=cart_count)
