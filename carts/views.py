3509fe8820b83cf162aaccb0d7a5780001d776b0
ayush-m1911 <ayushmetkar3@gmail.com>
2025-12-25 16:14:22 +0530

    Paginator now works smoothly

--- carts/views.py
+++ carts/views.py
@@ -53,6 +53,8 @@ def delete_cart_item(request, product_id):
 
 def cart(request,total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id = _cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
--- store/views.py
+++ store/views.py
@@ -3,20 +3,27 @@ from category.models import Category
 from .models import Product
 from carts.models import CartItem, Cart
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def store(request, category_slug=None):
     categories = None
     products = None
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        products = Product.objects.filter(category=categories, is_available=True).order_by('id')
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-       products = Product.objects.all().filter(is_available=True)
+       products = Product.objects.all().filter(is_available=True).order_by('id')
+       paginator = Paginator(products, 3)
+       page = request.GET.get('page')
+       paged_products = paginator.get_page(page)
        product_count = products.count()
 
     context = {
-         'products': products,
+         'products': paged_products,
         'product_count': product_count,
 
     }
