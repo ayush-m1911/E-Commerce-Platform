caf9bfa19a182e26cda4bb6f503e75d82f4e8888
ayush-m1911 <ayushmetkar3@gmail.com>
2025-12-25 18:06:01 +0530

    Product can be selected on the basis of Sizes and Colors

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 # Create your views here.
 def _cart_id(request):
@@ -10,6 +10,18 @@ def _cart_id(request):
     return cart
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == 'POST':
+      for item in request.POST:
+          key = item
+          value = request.POST[key]
+          
+          try:
+              variation = Variation.objects.get(product = product, variation_category__iexact=key, variation_value__iexact=value)
+              product_variation.append(variation)
+          except:
+                pass
+
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))
         cart.save()
