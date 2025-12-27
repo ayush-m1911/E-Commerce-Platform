0ecb8e052976f442b1d631721e599c6826784578
ayush-m1911 <ayushmetkar3@gmail.com>
2025-12-27 11:26:29 +0530

    Registration Page was created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Account
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            confirm_password = form.cleaned_data['confirm_password']
+
+            
+            username = email.split('@')[0]
+
+            user = Account.objects.create_user(
+                    first_name=first_name,
+                    last_name=last_name,
+                    username=username,
+                    email=email,
+                    password=password
+                )
+            user.phone_number = phone_number
+            user.save()
+
+                # ✅ Redirect after success
+            return redirect('login')
+
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('store')
