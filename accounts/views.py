8650c8f89465727f419db0eb85fd14eeb564ac60
ayush-m1911 <ayushmetkar3@gmail.com>
2026-01-11 10:21:29 +0530

    Account activation of user started!

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -28,6 +35,19 @@ def register(request):
                 )
             user.phone_number = phone_number
             user.save()
+
+            #USER Activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                                           'user': user,
+                                           'domain': current_site,
+                                           'uid': urlsafe_base64_encode(force_bytes(user.pk)), 
+                                           'token': default_token_generator.make_token(user),                                         
+                                       })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
             messages.success(request, 'Registration successful. You can now log in.')
                 # ✅ Redirect after success
             return redirect('register')
