b29a76a6e4a3ed722574a6a36ed53af3aef23ed0
ayush-m1911 <ayushmetkar3@gmail.com>
2026-01-16 19:23:34 +0530

    Verification Email can be sent to the user now

--- goodmart/settings.py
+++ goodmart/settings.py
@@ -141,7 +141,7 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ayushmetkar422@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ayush@422'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+
+EMAIL_HOST_USER = 'ayushmetkar3@gmail.com'
+EMAIL_HOST_PASSWORD = 'pjge owmx kugv qpjw'
