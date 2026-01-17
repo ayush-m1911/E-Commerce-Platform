fd05cf8863a92924c367bf139f4846201b1f417c
ayush-m1911 <ayushmetkar3@gmail.com>
2026-01-17 18:40:08 +0530

    User Dashboard added!

--- accounts/views.py
+++ accounts/views.py
@@ -66,7 +66,7 @@ def login(request):
        if user is not None:
            auth.login(request, user)
            #messages.success(request, 'You are now logged in.')
-           return redirect('home')
+           return redirect('dashboard')
        else:
            messages.error(request, 'Invalid login credentials')
            return redirect('login')       
@@ -92,3 +92,7 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Invalid activation link')
         return redirect('register')
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
