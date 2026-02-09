d3adea51923806152c1322b02cf67c35a0b07688
ayush-m1911 <ayushmetkar3@gmail.com>
2026-02-09 19:31:37 +0530

    Changes in viees.py for Order App

--- order/views.py
+++ order/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from carts.models import CartItem
+from django.shortcuts import redirect
+from .forms import OrderForm
 # Create your views here.
+def place_order(request):
+    current_user = request.user
+
+    # if the cart count is less than or equal to 0, then redirect back to shop
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+    
+    if request.method == 'POST':
+        form  = OrderForm(request.POST)
+        
